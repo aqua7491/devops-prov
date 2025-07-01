7f1a266fa805044a8d3ba5f1096cb3daca079528
Oleg <oleg.oleg@oleg.oleg>
2025-07-01 16:31:45 +0300

    unittest fix

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ from unittest.mock import patch
 from io import StringIO
 from app import call
 
+
 class TestMyFunction(unittest.TestCase):
     @patch('sys.stdout', new_callable=StringIO)
     def test_output_content(self, mock_stdout):
         call()
         self.assertEqual(mock_stdout.getvalue(), "Hello, World!\n")
-
 
 
 if __name__ == '__main__':
